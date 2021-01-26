23be8286abe6f26639adb4d09d59e5c27a8b5132
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-26 08:04:26 +0100

    more code

--- example/02_helloworld/gen00.lisp
+++ example/02_helloworld/gen00.lisp
@@ -9,7 +9,7 @@
 
 (in-package :cl-csharp-generator)
 
-
+;; https://www.mono-project.com/docs/getting-started/mono-basics/
 (progn
   (defparameter *source-dir* #P"example/02_helloworld/source/")
 
@@ -28,6 +28,65 @@
 		       (public))
 	      (Console.WriteLine (string "Hello World from Mono!")))))))
 
+
+(write-source
+   (asdf:system-relative-pathname
+    'cl-csharp-generator
+    (merge-pathnames
+     #P"run_02_https_get.cs"
+     *source-dir*))
+   `(do0
+     (using System
+	    System.Net)
+     (defclass HttpsTest ()
+	    (declare (public))
+	    (defmethod Main ()
+	      (declare (static)
+		       (public))
+	      (let ((c (new (System.Net.WebClient))))
+		(Console.WriteLine
+		 (c.DownloadString (string "https://www.nuget.org"))))))))
+
+
+(write-source
+   (asdf:system-relative-pathname
+    'cl-csharp-generator
+    (merge-pathnames
+     #P"run_03_winforms.cs"
+     *source-dir*))
+   `(do0
+     (using System
+	    System.Windows.Forms)
+     (defclass WinFormsTest (Form)
+	    (declare (public))
+	    (defmethod Main ()
+	      (declare (static)
+		       (public))
+	      (Application.Run (new (WinFormsTest))))
+	    (defmethod WinFormsTest ()
+	      (declare (values :constructor))
+	      (setf Text (string "helloworld"))))))
+
+(write-source
+   (asdf:system-relative-pathname
+    'cl-csharp-generator
+    (merge-pathnames
+     #P"run_04_gtksharp.cs"
+     *source-dir*))
+   `(do0
+     (using Gtk
+	    System
+	    )
+     (defclass GtkTest ()
+	    (declare (public))
+	    (defmethod Main ()
+	      (declare (static)
+		       (public))
+	      (Application.Init)
+	      (let ((win (new (Window (string "hello mono gtksharp")))))
+		(win.Show)
+		(Application.Run))))))
+;; mcs run_04_gtksharp.cs -pkg:gtk-sharp-2.0
 
 
 ;; csc *.cs
