2275a201382f1a329f0b0a1b92942743ba11cbe7
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-26 07:59:06 +0100

    gtk window opens

---
+++ example/02_helloworld/source/run_03_winforms.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows.Forms;
+public class WinFormsTest  : Form {
+        public static void Main (){
+                Application.Run(new WinFormsTest());
+}
+        public  WinFormsTest (){
+                        Text="helloworld";
+}
+}
