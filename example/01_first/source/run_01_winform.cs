96d952c1d8284379bc7a83bc2e720660ca90b7f7
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-26 07:16:16 +0100

    add the generated file

---
+++ example/01_first/source/run_01_winform.cs
@@ -0,0 +1,10 @@
+using(System, System.Drawing, System.Windows.Forms);
+class(HelloWorld, Form(), declare(public ), public void Main (){
+        Application.Run(new HelloWorld());
+}, void HelloWorld (){
+            var b  = new Button();
+        b.Text="Click Me!";
+    (b.Click)+=(new EventHandler(ButtonClick));
+}, private void ButtonClick (object sender, EventArgs e){
+        MessageBox.Show("Button Clicked!");
+});
