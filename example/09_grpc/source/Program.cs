d181379837a8c74e4fbf754be6effc8899c233b2
Martin Kielhorn <kielhorn.martin@gmail.com>
2023-01-21 09:52:06 +0100

    add some generated files

---
+++ example/09_grpc/source/Program.cs
@@ -0,0 +1,11 @@
+using source.Services;
+var builder  = WebApplication.CreateBuilder(args);
+builder.Services().AddGrpc(( options)=> {
+        options.Credentials=ChannelCredentials.Insecure;
+});
+var app  = builder.Build;
+app.MapGrpcService<GreeterService>();
+app.MapGet(/, ()=> {
+        Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
+});
+app.Run;
