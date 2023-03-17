9efcbd813922a36b9234cf2cf528e9571affad9b
Martin Kielhorn <martin.kielhorn@ideapad>
2023-03-17 23:38:08 +0100

    the example is still not compiling

--- example/10_logconf/source/setup01_init.sh
+++ example/10_logconf/source/setup01_init.sh
@@ -2,3 +2,8 @@ dotnet new console -n ConsoleApp
 dotnet new sln -n ConsoleApp
 dotnet sln add ConsoleApp/ConsoleApp.csproj
 
+cd ConsoleApp
+dotnet add package Microsoft.Extensions.Configuration
+dotnet add package Microsoft.Extensions.Logging
+dotnet add package Microsoft.Extensions.Configuration.Json
+dotnet add package Microsoft.Extensions.Configuration.CommandLine
