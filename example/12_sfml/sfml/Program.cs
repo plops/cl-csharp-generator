147d1bf171107378df68e1fb8a6a5866f66bd155
Martin Kielhorn <martin.kielhorn@ideapad>
2023-05-31 23:41:21 +0200

    add sfml example

---
+++ example/12_sfml/sfml/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace SFMLNetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the main window
+            var window = new RenderWindow(new VideoMode(800, 600), "SFML.Net Window");
+
+            // Create a circle
+            var circle = new CircleShape(50f)
+            {
+                FillColor = Color.Red,
+                Position = new Vector2f(50f, 50f)
+            };
+
+            // Start the game loop
+            while (window.IsOpen)
+            {
+                // Process events
+                window.DispatchEvents();
+
+                // Clear screen
+                window.Clear();
+
+                // Draw the circle
+                window.Draw(circle);
+
+                // Update the window
+                window.Display();
+            }
+        }
+    }
+}
---
+++ example/12_sfml/sfml/README.md
@@ -0,0 +1,7 @@
+
+- create the code
+```
+dotnet new console
+dotnet add package SFML.Net --version 2.5.0
+pacman -Ss csfml
+```
