40bd68e7e0b86cf6c719228045a199390be677fa
Martin Kielhorn <martin.kielhorn@ideapad>
2023-06-07 21:40:20 +0200

    format code

--- example/12_sfml/gen01.lisp
+++ example/12_sfml/gen01.lisp
@@ -16,15 +16,7 @@
     '("Monday" "Tuesday" "Wednesday"
       "Thursday" "Friday" "Saturday"
       "Sunday"))
-  (defun lprint (&key (msg "") vars)
-      `(Console.WriteLine
-	(string$ ,(format nil "~a~{ ~a~}"
-			  msg
-			  (loop for e in vars
-				collect
-				(format nil "~a='{~a}'"
-					(emit-cs :code e)
-					(emit-cs :code e)))))))
+  
   (let ((fn (merge-pathnames
 	     (format nil  "~a.csproj" project)
 	     *source-dir*)))
@@ -92,96 +84,72 @@
 		      (out "<PrivateAssets>all</PrivateAssets>")
 		      (out "</PackageReference>")))
 	  (out "</ItemGroup>"))
+	(out "</Project>"))))
+  (let ((name 'Program))
+    (defun lprint (&key (msg "") vars)
+      `(Console.WriteLine
+	(string$ ,(format nil "~a~{ ~a~}"
+			  msg
+			  (loop for e in vars
+				collect
+				(format nil "~a='{~a}'"
+					(emit-cs :code e)
+					(emit-cs :code e)))))))
+    (write-source
+     (merge-pathnames
+      (format nil "~a.cs" name)
+      *source-dir*)
+     `(do0
+       (comments "<copyright file=\"Program.cs\" company=\"Plops\">"
+		 "Copyleft (c) 2023 Plops. This file is MIT Licensed."
+		 "</copyright>")
+       
+       (do0
+	(namespace
+	 ,(format nil "~a" project)
+  	 (using	
+	  SFML.Graphics
+	  SFML.System
+	  SFML.Window)
+	 ""
+	 (defclass ,name ()
+	   (defmethod Main (args)
+	     (declare (type "string[]" args)
+		      (static))
+	     (do0
+	      ,(lprint :msg (multiple-value-bind
+				  (second minute hour date month year day-of-week dst-p tz)
+				(get-decoded-time)
+			      (declare (ignorable dst-p))
+			      (format nil "code generation on: ~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+				      hour
+				      minute
+				      second
+				      (nth day-of-week *day-names*)
+				      year
+				      month
+				      date
+				      (- tz))))
+	      (let ((window (new (RenderWindow (new (VideoMode 800 600))
+					       (string "SFML.Net Window"))))
+		    (circle (new (CircleShape 50f))))
+		(setf circle.FillColor Color.Red
+		      
+		      circle.Position (new (Vector2f 50f 50f)))
+		(let ((clock (new (Clock)))
+		      (baseRadius 50f)
+		      (pulseSpeed (cast float (/ (* 2 Math.PI )
+						 1.3))))
 
-	
-	(out "</Project>"))))1
-
-  (let ((l-conf `((:name executable :short x :type string? :default (string "/usr/bin/ls") :required nil)
-		  (:name log-file :short f :type string? ; :default (string "")
-		   :required nil)
-		  (:name debug-level :short l :type string? ;:default (string "")
-		   :required nil
-			 )
-		  (:name config-file :short c :type string? :default (string "appSettings.json") :required nil :config nil)
-		  (:name host-name :short h :type string? ;:default (string "")
-		   :required nil :config nil)
-		  (:name port :short p :type int? :required nil :config nil)
-		  ;(:name help :short h :type bool :default false :required nil :config nil)
-		  )
-		))
-   (let ((name 'Program))
-     (write-source
-      (merge-pathnames
-       (format nil "~a.cs" name)
-       *source-dir*)
-     
-      `(do0
-	(comments 
-		  "<copyright file=\"Program.cs\" company=\"Plops\">"
-		  "Copyleft (c) 2023 Plops. This file is MIT Licensed."
-		  "</copyright>"
-		  )
-	
-	(do0
-	 (namespace
-	  ,(format nil "~a" project)
-
-  	  (using				; System
-	 SFML.Graphics
-	 SFML.System
-	 SFML.Window)
-	  ""
-	  (defclass ,name ()
-	    
-	    (defmethod Main (args)
-	      (declare (type "string[]" args)
-		       (static))
-	      (do0
-	       ,(lprint :msg (multiple-value-bind
-				   (second minute hour date month year day-of-week dst-p tz)
-				 (get-decoded-time)
-			       (declare (ignorable dst-p))
-			       (format nil "code generation on: ~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
-				       hour
-				       minute
-				       second
-				       (nth day-of-week *day-names*)
-				       year
-				       month
-				       date
-				       (- tz))))
-
-	       
-	       
-	       (let ((window (new (RenderWindow (new (VideoMode 800 600))
-						(string "SFML.Net Window"))))
-		     (circle (new (CircleShape 50f))))
-		 (setf circle.FillColor Color.Red
-		       
-		       circle.Position (new (Vector2f 50f 50f)))
-		 (let ((clock (new (Clock)))
-		       (baseRadius 50f)
-		       (pulseSpeed (cast float (/ (* 2 Math.PI )
-						  1.3))))
-
-		     (while window.IsOpen
-			    (window.DispatchEvents)
-			    (window.Clear)
-			    (let ((time (clock.ElapsedTime.AsSeconds)))
-			     (setf circle.Radius
-				   (* baseRadius
-				      (/ (+ 1 (cast float
+		  (while window.IsOpen
+			 (window.DispatchEvents)
+			 (window.Clear)
+			 (let ((time (clock.ElapsedTime.AsSeconds)))
+			   (setf circle.Radius
+				 (* baseRadius
+				    (/ (+ 1 (cast float
 						  (Math.Sin (* pulseSpeed
 							       time))))
-					 2))))
-			    (window.Draw circle)
-			    (window.Display))))
-	      
-	       )))))))))
-  ;;dotnet publish -c Release
-  #+nil (sb-ext:run-program "/usr/bin/dotnet"
-		      `("format")
-		      :directory *source-dir*
-		      :wait t)
-  
-  )
+				       2))))
+			 (window.Draw circle)
+			 (window.Display)))))))))))))
--- example/12_sfml/source01/SfmlExample/Program.cs
+++ example/12_sfml/source01/SfmlExample/Program.cs
@@ -1,29 +1,33 @@
 // <copyright file="Program.cs" company="Plops">
 // Copyleft (c) 2023 Plops. This file is MIT Licensed.
 // </copyright>
-namespace SfmlExample {
-        using SFML.Graphics;
-using SFML.System;
-using SFML.Window;
-        
-        public class Program {
-                public static void Main (string[] args){
-                                    Console.WriteLine($"code generation on: 21:24:26 of Wednesday, 2023-06-07 (GMT+1)");
-                        var window  = new RenderWindow(new VideoMode(800, 600), "SFML.Net Window");
-            var circle  = new CircleShape(50f);
-                        circle.FillColor=Color.Red;
-            circle.Position=new Vector2f(50f, 50f);
-                        var clock  = new Clock();
-            var baseRadius  = 50f;
-            var pulseSpeed  = (float) ((((2)*(Math.PI)))/((1.30f)));
-            while (window.IsOpen) {
-                                window.DispatchEvents();
-                                window.Clear();
-                                                var time  = clock.ElapsedTime.AsSeconds();
-                                circle.Radius=((baseRadius)*(((((1)+((float) Math.Sin(((pulseSpeed)*(time))))))/(2))));
-                                window.Draw(circle);
-                                window.Display();
-}
-}
-}
+namespace SfmlExample
+{
+    using SFML.Graphics;
+    using SFML.System;
+    using SFML.Window;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"code generation on: 21:24:26 of Wednesday, 2023-06-07 (GMT+1)");
+            var window = new RenderWindow(new VideoMode(800, 600), "SFML.Net Window");
+            var circle = new CircleShape(50f);
+            circle.FillColor = Color.Red;
+            circle.Position = new Vector2f(50f, 50f);
+            var clock = new Clock();
+            var baseRadius = 50f;
+            var pulseSpeed = (float)((((2) * (Math.PI))) / ((1.30f)));
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear();
+                var time = clock.ElapsedTime.AsSeconds();
+                circle.Radius = ((baseRadius) * (((((1) + ((float)Math.Sin(((pulseSpeed) * (time)))))) / (2))));
+                window.Draw(circle);
+                window.Display();
+            }
+        }
+    }
 }
